f788c1a6c012b6ec790001bfd56860b02949978f
Fahad Ullah Khan <fahadullahk4@gmail.com>
2024-04-28 20:17:38 +0500

    Completed

--- main.js
+++ main.js
@@ -9,7 +9,35 @@ const throttleFunction = (func, delay) => {
 		}
 	};
 };
-document.querySelector(".center").addEventListener(
+document.querySelector(".center h1").addEventListener(
 	"mousemove",
-	throttleFunction(() => {}, 400)
+	throttleFunction((dets) => {
+		var div = document.createElement("div");
+		div.classList.add("image-div");
+		div.style.left = dets.clientX + "px";
+		div.style.top = dets.clientY + "px";
+		document.body.appendChild(div);
+
+		var img = document.createElement("img");
+		img.setAttribute(
+			"src",
+			"https://plus.unsplash.com/premium_photo-1669837324386-1813fcc28947?q=80&w=2565&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+		);
+		div.appendChild(img);
+
+		gsap.to("img", {
+			y: "0",
+			ease: Power1,
+			duration: .6,
+		});
+		gsap.to("img", {
+			y: "100%",
+			ease: Power2,
+			delay: 0.6,
+		});
+
+		setTimeout(() => {
+			div.remove();
+		}, 2000);
+	}, 400)
 );
